d7d07790efca0e61d54d5f823dd9114437ba7093
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-09-30 18:03:17 +0700

    4.3

--- src/main/java/se331/lab/rest/config/InitApp.java
+++ src/main/java/se331/lab/rest/config/InitApp.java
@@ -136,6 +136,6 @@ public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
-        
+
     }
 }
